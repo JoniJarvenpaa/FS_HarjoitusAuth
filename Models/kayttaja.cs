b3639ff4327800dc861e360c366563943720d4cf
JoniJarvenpaa <110594602+JoniJarvenpaa@users.noreply.github.com>
2025-04-01 14:21:49 +0300

    Delete Models/kayttaja.cs

--- Models/kayttaja.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SecondMVC.Models
-{
-    public class kayttaja
-    {
-        public string username { get; set; }
-        public string password { get; set; }
-        public kayttaja() { }
-    }
-}
