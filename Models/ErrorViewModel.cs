acf9b0175536f5f9c2ff0e7f40a7b3fbdb26a351
JoniJarvenpaa <110594602+JoniJarvenpaa@users.noreply.github.com>
2025-04-02 13:36:41 +0300

    Fix'd

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
